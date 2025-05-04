92b3b6d366f08a65bef9da9c67f46c24c5e50be4
unknown <er.irfan041@gmail.com>
2025-05-04 07:27:56 +0530

    Project added

---
+++ src/main/java/com/example/bootwithtesting/BootwithtestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootwithtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootwithtestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootwithtestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bootwithtesting/BootwithtestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootwithtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootwithtestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
